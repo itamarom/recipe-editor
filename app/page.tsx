de36a7a421f62ffb6e05506e871b61245965da81
Itamar Marom <itamarnine4@gmail.com>
2024-10-19 15:50:50 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -129,7 +129,7 @@ const MainPage = () => {
 
     const recipe = SavedRecipeSchema.parse(JSON.parse(saved_query as string));
     setFromSavedRecipe(recipe);
-    router.replace(pathname);
+    // router.replace(pathname);
 
   }, [params, pathname, router]);
 
